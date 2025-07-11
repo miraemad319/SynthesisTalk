f6ae9b2d18a826762d4360a775ada75059d87ebd
miraemad319 <mira.emad2004@gmail.com>
2025-07-11 10:18:20 +0300

    Connected to database.

--- backend/services/db_session.py
+++ backend/services/db_session.py
@@ -8,11 +8,17 @@ if settings.DATABASE_URL:
 else:
     db_url = f"postgresql://{settings.USERNAME}:{settings.PASSWORD}@{settings.HOST}:{settings.PORT}/{settings.DB_NAME}"
 
-engine = create_engine(db_url, echo=True)
+engine = create_engine(
+    db_url,
+    echo=True,
+    pool_size=3,         # Lower pool size to avoid hitting free-tier limits
+    max_overflow=0,      # Do not allow more than pool_size connections
+)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 def get_session():
-    return Session(engine)
+    with Session(engine) as session:
+        yield session
 
